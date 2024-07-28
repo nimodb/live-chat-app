0510592dd6ed8cd48feb58cd6135991d7146f48d
Nima Saadati <nimodbdev@gmail.com>
2024-07-28 22:38:44 +0400

    Configure channel layer with InMemoryChannelLayer and update ChatRoomConsumer to support group messaging. Implement group add and discard, and handle message broadcasting with group_send.

--- core/settings.py
+++ core/settings.py
@@ -86,6 +86,12 @@ TEMPLATES = [
 # WSGI_APPLICATION = "core.wsgi.application"
 ASGI_APPLICATION = "core.asgi.application"
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    }
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
--- rtchat/consumers.py
+++ rtchat/consumers.py
@@ -2,6 +2,7 @@ import json
 from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 from .models import ChatGroup, GroupMessage
 
 
@@ -10,7 +11,17 @@ class ChatRoomConsumer(WebsocketConsumer):
         self.user = self.scope["user"]
         self.chatroom_name = self.scope["url_route"]["kwargs"]["chatroom_name"]
         self.chatroom = get_object_or_404(ChatGroup, group_name=self.chatroom_name)
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.chatroom_name, self.channel_name
+        )
+
         self.accept()
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.chatroom_name, self.channel_name
+        )
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -21,6 +32,15 @@ class ChatRoomConsumer(WebsocketConsumer):
             author=self.user,
             group=self.chatroom,
         )
+        event = {
+            "type": "message_handler",
+            "message_id": message.id,
+        }
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
+
+    def message_handler(self, event):
+        message_id = event["message_id"]
+        message = GroupMessage.objects.get(id=message_id)
         context = {"message": message, "user": self.user}
         html = render_to_string("rtchat/partials/chat_message_p.html", context=context)
         self.send(text_data=html)
