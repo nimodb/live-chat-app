01fd69d1ce9d68e7b699036dd0c51c0c8acba1c8
Nima Saadati <nimodbdev@gmail.com>
2024-07-28 22:20:41 +0400

    Update ChatRoomConsumer to handle message reception and broadcasting. Add logic to create GroupMessage instances and render HTML for new messages.

--- rtchat/consumers.py
+++ rtchat/consumers.py
@@ -1,6 +1,26 @@
+import json
+from django.shortcuts import get_object_or_404
+from django.template.loader import render_to_string
 from channels.generic.websocket import WebsocketConsumer
+from .models import ChatGroup, GroupMessage
 
 
 class ChatRoomConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope["user"]
+        self.chatroom_name = self.scope["url_route"]["kwargs"]["chatroom_name"]
+        self.chatroom = get_object_or_404(ChatGroup, group_name=self.chatroom_name)
         self.accept()
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        body = text_data_json["body"]
+
+        message = GroupMessage.objects.create(
+            body=body,
+            author=self.user,
+            group=self.chatroom,
+        )
+        context = {"message": message, "user": self.user}
+        html = render_to_string("rtchat/partials/chat_message_p.html", context=context)
+        self.send(text_data=html)
