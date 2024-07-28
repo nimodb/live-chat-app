f0ab2fd9d1a6418a160f7a9d122273994eeb1160
Nima Saadati <nimodbdev@gmail.com>
2024-07-28 23:32:43 +0400

    Add real-time online user tracking to ChatRoomConsumer. Update ChatGroup model with `users_online` field, implement logic to manage online users, and broadcast online count updates. Include animation for online count display in `online_count.html`.

--- rtchat/consumers.py
+++ rtchat/consumers.py
@@ -16,12 +16,22 @@ class ChatRoomConsumer(WebsocketConsumer):
             self.chatroom_name, self.channel_name
         )
 
+        # add and update online users
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()
+
         self.accept()
 
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
             self.chatroom_name, self.channel_name
         )
+
+        # remove and update online users
+        if self.user in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count()
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -43,4 +53,19 @@ class ChatRoomConsumer(WebsocketConsumer):
         message = GroupMessage.objects.get(id=message_id)
         context = {"message": message, "user": self.user}
         html = render_to_string("rtchat/partials/chat_message_p.html", context=context)
+        self.send(text_data=html)
+
+    def update_online_count(self):
+        online_count = self.chatroom.users_online.count() - 1
+        event = {
+            "type": "online_count_handler",
+            "online_count": online_count,
+        }
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
+
+    def online_count_handler(self, event):
+        online_count = event["online_count"]
+
+        context = {"online_count": online_count}
+        html = render_to_string("rtchat/partials/online_count.html", context=context)
         self.send(text_data=html)
--- rtchat/models.py
+++ rtchat/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=128, unique=True, blank=True)
+    users_online = models.ManyToManyField(
+        User, related_name="online_in_groups", blank=True
+    )
 
     def __str__(self):
         return self.group_name
